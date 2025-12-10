59be188a68e5de3dd8045bf9272961600af00309
vahid mohamadi <31723327+vaeid@users.noreply.github.com>
2025-12-10 09:29:03 +0330

    edit register controller

--- register_controll.php
+++ register_controll.php
@@ -9,48 +9,57 @@
 
 <body>
     <?php
-    $variable = $_POST["swi"];
-    switch ($variable) {
-        case '2':
 
-            $captcha = $_POST["captcha"];
-            if ($captcha == 45) {
-                $pass1 = $_POST["pass1"];
-                $pass2 = $_POST["pass2"];
-                if ($pass1 == $pass2) {
-                    echo  $_POST["username"];
-                    echo "<br>";
-                    echo  $_POST["pass1"];
-                    echo "<br>";
-                    echo  $_POST["f_name"];
-                    echo "<br>";
-                    echo  $_POST["l_name"];
-                    echo "<br>";
-                    echo  $_POST["day"];
-                    echo "<br>";
-                    echo  $_POST["month"];
-                    echo "<br>";
-                    echo  $_POST["year"];
-                } else {
-                    echo '<p style="color:red">   رمز عبور یکسان نمی باشد ، دقققت کن!!!</p>';
-                }
-            } else {
-                echo '<p style="color:red"> سوال امنیتی اشتباه می باشد ، دقققت کن!!!</p>';
+    if (isset($_POST["swi"])) {
+        if (empty($_POST["swi"])) {
+            echo "swi is set and empty ";
+        } else {
+            $variable = $_POST["swi"];
+            switch ($variable) {
+                case '2':
+
+                    $captcha = $_POST["captcha"];
+                    if ($captcha == 45) {
+                        $pass1 = $_POST["pass1"];
+                        $pass2 = $_POST["pass2"];
+                        if ($pass1 == $pass2) {
+                            echo  $_POST["username"];
+                            echo "<br>";
+                            echo  $_POST["pass1"];
+                            echo "<br>";
+                            echo  $_POST["f_name"];
+                            echo "<br>";
+                            echo  $_POST["l_name"];
+                            echo "<br>";
+                            echo  $_POST["day"];
+                            echo "<br>";
+                            echo  $_POST["month"];
+                            echo "<br>";
+                            echo  $_POST["year"];
+                        } else {
+                            echo '<p style="color:red">   رمز عبور یکسان نمی باشد ، دقققت کن!!!</p>';
+                        }
+                    } else {
+                        echo '<p style="color:red"> سوال امنیتی اشتباه می باشد ، دقققت کن!!!</p>';
+                    }
+
+                    break;
+                case '3':
+                    echo "B";
+                    break;
+                case '4':
+                    echo "C";
+                    break;
+                case '5':
+                    echo "E";
+                    break;
+                default:
+                    echo "unKnow";
+                    break;
             }
-
-            break;
-        case '3':
-            echo "B";
-            break;
-        case '4':
-            echo "C";
-            break;
-        case '5':
-            echo "E";
-            break;
-        default:
-            echo "unKnow";
-            break;
+        }
+    } else {
+        echo "swi is not set ";
     }
 
 
